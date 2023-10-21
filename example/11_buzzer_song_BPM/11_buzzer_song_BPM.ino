607b9af5513aa4186ad0eb720cecd96ef134d2de
HP14000 <148601170+HP14000@users.noreply.github.com>
2023-10-21 15:49:04 +0800

    update

--- example/11_buzzer_song_BPM/11_buzzer_song_BPM.ino
+++ example/11_buzzer_song_BPM/11_buzzer_song_BPM.ino
@@ -1,60 +1,95 @@
-#include "notes.h"        // 引入函式庫
 
-int buzzerPin = 9;        // 定義蜂鳴器引腳
-int size;
-float BPM = 120.0;                // Beats Per minute
-int quarter = (60/BPM)*1000;      // 四分音符
-int half = quarter * 2;           // 半分音符
-int eigth = quarter / 2;          // 八分音符
+#define NOTE_D0 147
+#define NOTE_D1 294 
+#define NOTE_D2 330 
+#define NOTE_D3 350 
+#define NOTE_D4 393 
+#define NOTE_D5 441 
+#define NOTE_D6 495 
+#define NOTE_D7 556 
 
-void set_beats(float BPM, int beats[]) {
-  float BPS;
-  BPS = BPM / 60.0;               // 換成 Beats Per second
-  beats[1] = round(BPS * 1000);
-  beats[0] = round(beats[1]*2);
-  beats[2] = round(beats[1]/2);
-}
+#define NOTE_DL1 147 
+#define NOTE_DL2 165 
+#define NOTE_DL3 175 
+#define NOTE_DL4 196 
+#define NOTE_DL5 221 
+#define NOTE_DL6 248 
+#define NOTE_DL7 278 
 
-// 簡譜對照表
-// 1: C4, 2: D4, 3: E4, 4: F4, 5: G4, 6: A4
-// 5. : G3
+#define NOTE_DH1 589 
+#define NOTE_DH2 661 
+#define NOTE_DH3 700 
+#define NOTE_DH4 786 
+#define NOTE_DH5 882 
+#define NOTE_DH6 990 
+#define NOTE_DH7 112  
+//以上部分是定義是把每個音符和頻率值對應起來，其實不用打這麼多，但是都打上了，後面可以隨意編寫
 
-// 用陣列儲存兩隻老虎的旋律
-int melody[] = {NOTE_C4, NOTE_D4, NOTE_E4, NOTE_C4,
-                NOTE_C4, NOTE_D4, NOTE_E4, NOTE_C4,
-                NOTE_E4, NOTE_F4, NOTE_G4,
-                NOTE_E4, NOTE_F4, NOTE_G4,
-                NOTE_G4, NOTE_A4, NOTE_G4, NOTE_F4, NOTE_E4, NOTE_C4,
-                NOTE_G4, NOTE_A4, NOTE_G4, NOTE_F4, NOTE_E4, NOTE_C4,
-                NOTE_D4, NOTE_G3, NOTE_C4,
-                NOTE_D4, NOTE_G3, NOTE_C4
-                };
+#define WHOLE 1 
+#define HALF 0.5 
+#define QUARTER 0.25 
+#define EIGHTH 0.25 
+#define SIXTEENTH 0.625  
+//這部分是用英文對應了拍子，這樣後面也比較好看
 
-// 用陣列儲存兩隻老虎的節奏
-int noteDurations[] = { quarter, quarter, quarter, quarter,
-                        quarter, quarter, quarter, quarter,
-                        quarter, quarter, half,
-                        quarter, quarter, half,
-                        eigth, eigth, eigth, eigth, quarter, quarter,
-                        eigth, eigth, eigth, eigth, quarter, quarter,
-                        quarter, quarter, half,
-                        quarter, quarter, half
-                      };
 
-void setup() {
-  pinMode(buzzerPin, OUTPUT);                 // 設置蜂鳴器腳位為輸出
-  Serial.begin(9600);
-  size = sizeof(melody) / sizeof(melody[0]);  // 計算儲存整首歌陣列的大小
-}
+int tune[] =  
+{  
+ NOTE_D0,NOTE_D0,NOTE_D0,NOTE_D6,NOTE_D7,NOTE_DH1,NOTE_D7,NOTE_DH1,NOTE_DH3,NOTE_D7,NOTE_D7,NOTE_D7,NOTE_D3,NOTE_D3,
+ NOTE_D6,NOTE_D5,NOTE_D6,NOTE_DH1,NOTE_D5,NOTE_D5,NOTE_D5,NOTE_D3,NOTE_D4,NOTE_D3,NOTE_D4,NOTE_DH1,
+ NOTE_D3,NOTE_D3,NOTE_D0,NOTE_DH1,NOTE_DH1,NOTE_DH1,NOTE_D7,NOTE_D4,NOTE_D4,NOTE_D7,NOTE_D7,NOTE_D7,NOTE_D0,NOTE_D6,NOTE_D7,
+ NOTE_DH1,NOTE_D7,NOTE_DH1,NOTE_DH3,NOTE_D7,NOTE_D7,NOTE_D7,NOTE_D3,NOTE_D3,NOTE_D6,NOTE_D5,NOTE_D6,NOTE_DH1,
+ NOTE_D5,NOTE_D5,NOTE_D5,NOTE_D2,NOTE_D3,NOTE_D4,NOTE_DH1,NOTE_D7,NOTE_D7,NOTE_DH1,NOTE_DH1,NOTE_DH2,NOTE_DH2,NOTE_DH3,NOTE_DH1,NOTE_DH1,NOTE_DH1,
+ NOTE_DH1,NOTE_D7,NOTE_D6,NOTE_D6,NOTE_D7,NOTE_D5,NOTE_D6,NOTE_D6,NOTE_D6,NOTE_DH1,NOTE_DH2,NOTE_DH3,NOTE_DH2,NOTE_DH3,NOTE_DH5,
+ NOTE_DH2,NOTE_DH2,NOTE_DH2,NOTE_D5,NOTE_D5,NOTE_DH1,NOTE_D7,NOTE_DH1,NOTE_DH3,NOTE_DH3,NOTE_DH3,NOTE_DH3,NOTE_DH3,
+ NOTE_D6,NOTE_D7,NOTE_DH1,NOTE_D7,NOTE_DH2,NOTE_DH2,NOTE_DH1,NOTE_D5,NOTE_D5,NOTE_D5,NOTE_DH4,NOTE_DH3,NOTE_DH2,NOTE_DH1,
+ NOTE_DH3,NOTE_DH3,NOTE_DH3,NOTE_DH3,NOTE_DH6,NOTE_DH6,NOTE_DH5,NOTE_DH5,NOTE_DH3,NOTE_DH2,NOTE_DH1,NOTE_DH1,NOTE_D0,NOTE_DH1,
+ NOTE_DH2,NOTE_DH1,NOTE_DH2,NOTE_DH2,NOTE_DH5,NOTE_DH3,NOTE_DH3,NOTE_DH3,NOTE_DH3,NOTE_DH6,NOTE_DH6,NOTE_DH5,NOTE_DH5,
+ NOTE_DH3,NOTE_DH2,NOTE_DH1,NOTE_DH1,NOTE_D0,NOTE_DH1,NOTE_DH2,NOTE_DH1,NOTE_DH2,NOTE_DH2,NOTE_D7,NOTE_D6,NOTE_D6,NOTE_D6,NOTE_D6,NOTE_D7
+};//這部分就是整首曲子的音符部分，用了一個序列定義爲tune，整數
 
-void loop() {
-  // 播放音樂
-  for (int i = 0; i < size; i++) {
-    int duration = noteDurations[i];
-    tone(buzzerPin, melody[i], duration);
-    int pauseBetweenNotes = duration * 1.30;
-    delay(pauseBetweenNotes);
-    noTone(buzzerPin);
-  }
-  delay(1000);  // 停頓一秒
+float duration[]= 
+
+{  
+  1,1,1,0.5,0.5,     1+0.5,0.5,1,1,     1,1,1,0.5,0.5,
+  1+0.5,0.5,1,1,     1,1,1,1,          1+0.5,0.5,1,1, 
+  1,1,0.5,0.5,0.5,0.5,    1+0.5,0.5,1,1,     1,1,1,0.5,0.5,
+  1+0.5,0.5,1,1,    1,1,1,0.5,0.5,     1+0.5,0.5,1,1,
+  1,1,1,0.5,0.5,    1,0.5,0.25,0.25,0.25,0.5,    0.5,0.5,0.5,0.25,0.5,1,
+  0.5,0.5,0.5,0.5,1,1,    1,1,1,0.5,0.5,    1+0.5,0.5,1,1,
+  1,1,1,0.5,0.5,    1.5,0.5,1,1,    1,1,1,1,
+  0.5,0.5,1,1,0.5,0.5,    1.5,0.25,0.5,1,    1,1,1,1,
+  1,1,1,1,    1,1,1,1,    0.5,0.5,1,1,0.5,0.5,
+  1,0.5,0.5,1,1,    1,1,1,1,    1,1,1,1,
+  0.5,0.5,1,1,0.5,0.5,    1,0.5,0.25,0.5,1,    1,1,1,0.5,0.5
+};//這部分是整首曲子的節拍部分，也定義個序列duration，浮點（數組的個數和前面音符的個數是一樣的，一一對應麼）
+
+int length;//這裏定義一個變量，後面用來表示共有多少個音符
+
+int tonePin=5;//蜂鳴器的pin 
+
+
+void setup() 
+
+{  
+  pinMode(tonePin,OUTPUT);//設置蜂鳴器的pin爲輸出模式
+
+  length = sizeof(tune)/sizeof(tune[0]);//這裏用了一個sizeof函數， 可以查出tone序列裏有多少個音符
+
+} 
+
+void loop() 
+
+{  
+
+  for(int x=0;x<length;x++)//循環音符的次數
+  {  
+    tone(tonePin,tune[x]);//此函數依次播放tune序列裏的數組，即每個 音符
+
+ delay(400*duration[x]);//每個音符持續的時間，即節拍duration，是調整時間的越大，曲子速度越慢，越小曲子速度越快，自己掌握吧
+
+noTone(tonePin);//停止當前音符，進入下一音符
+
+  } 
+  delay(5000);//等待5秒後，循環重新開始
 }
